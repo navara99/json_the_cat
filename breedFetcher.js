d660f0c5d1bac5765548cbc17cb4999299073b49
Tharsikan <thar.navaratnam@gmail.com>
2021-11-04 14:17:21 +0000

    allow user input

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,19 @@
 const request = require("request");
+const breed = process.argv.splice(2)[0];
 
-const endPoint = "https://api.thecatapi.com/v1/breeds/search?q=sib";
+const getBreedDetails = (breed) => {
+  const endPoint = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(endPoint,(err,res,body)=> {
-  const data = JSON.parse(body);
-  const description = data[0].description;
+  request(endPoint, (err, res, body) => {
+    if (err) return console.log("Error details: ", err);
 
-  console.log(description);
+    const data = JSON.parse(body);
 
-});
+    if (data.length === 0) return console.log(`Sorry, we could not find information for the breed ${breed}.`);
+
+    const description = data[0].description;
+    console.log(description);
+  });
+};
+
+getBreedDetails(breed)
