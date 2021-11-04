2e85416a91dbcadf57ef96244cca942805a09c20
Tharsikan <thar.navaratnam@gmail.com>
2021-11-04 14:22:02 +0000

    refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,22 @@
 const request = require("request");
 const breed = process.argv.splice(2)[0];
 
-const getBreedDetails = (breed) => {
+const getBreedDetails = (breed , errorHandler) => {
   const endPoint = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
   request(endPoint, (err, res, body) => {
-    if (err) return console.log("Error details: ", err);
+    if (err) return errorHandler(err,breed);
 
     const data = JSON.parse(body);
 
-    if (data.length === 0) return console.log(`Sorry, we could not find information for the breed ${breed}.`);
+    if (data.length === 0) return errorHandler(err, breed);
 
     const description = data[0].description;
     console.log(description);
   });
 };
 
-getBreedDetails(breed)
+getBreedDetails(breed , (err,breed)=> {
+  const output = err ? `Error details: ${err}` : `Sorry, we could not find information for the breed ${breed}.`
+  console.log(output);
+})
