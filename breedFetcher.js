cf8150a1e932f4c99de81b85226a4afd3589ce4f
Tharsikan <thar.navaratnam@gmail.com>
2021-11-04 14:00:00 +0000

    implement api request

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,11 @@
 const request = require("request");
 
+const endPoint = "https://api.thecatapi.com/v1/breeds/search?q=sib";
+
+request(endPoint,(err,res,body)=> {
+  const data = JSON.parse(body);
+  const description = data[0].description;
+
+  console.log(description);
+
+});
